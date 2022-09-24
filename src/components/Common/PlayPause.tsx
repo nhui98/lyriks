125a9097d450595667e336b8a432ba230035b67d
Nathan Hui <hui.n1998@gmail.com>
2022-09-24 19:25:33 +0100

    playpause

---
+++ src/components/Common/PlayPause.tsx
@@ -0,0 +1,25 @@
+import { Song } from "@store/services/shazamCore.types";
+import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
+
+const PlayPause = ({
+  song,
+  activeSong,
+  handlePause,
+  handlePlay,
+  isPlaying,
+}: PlayPauseProps) =>
+  isPlaying && activeSong?.title === song.title ? (
+    <FaPauseCircle size={35} className="text-gray-300" onClick={handlePause} />
+  ) : (
+    <FaPlayCircle size={35} className="text-gray-300" onClick={handlePlay} />
+  );
+
+export default PlayPause;
+
+interface PlayPauseProps {
+  isPlaying: boolean;
+  activeSong: Song | null;
+  song: Song;
+  handlePause: () => void;
+  handlePlay: () => void;
+}
